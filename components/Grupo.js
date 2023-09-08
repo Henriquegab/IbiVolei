2c0381bfb9157e92afdc6e72bbcd66bd4ff6a12f
henrique gabriel <henriquepro8@gmai.com>
2023-09-08 06:43:25 -0300

    api fetch gerenciar grupos

--- components/Grupo.js
+++ components/Grupo.js
@@ -1,8 +1,9 @@
 import { View, Text, TouchableOpacity, Image } from 'react-native'
 import React from 'react'
 import luis from '../assets/luis.png'
+import nicolas from '../assets/nicolas.jpg'
 
-const Grupo = () => {
+const Grupo = ({ nome, totalJogos, criado, jogos, membros, imagem }) => {
   return (
     
       
@@ -11,37 +12,37 @@ const Grupo = () => {
             
               {/* imagem */}
               <View className="w-16 h-16 rounded-full items-center justify-center pt-4 pl-6">
-                  <Image className="w-16 h-16 rounded-full" source={luis}></Image>
+                  <Image className="w-16 h-16 rounded-full" source={imagem ?? nicolas}></Image>
               </View>
               {/* Quadrado de informações grande */}
               <View className=" w-64 h-32 ml-6 mt-4 justify-between pb-2">
                 {/* titulo */}
                 <View className="">
-                  <Text className="text-white font-bold text-base text-left">Unidos do Desespero</Text>
+                  <Text className="text-white font-bold text-base text-left">{nome ?? '-'}</Text>
                 </View>
                 {/* Estatísticas */}
                 <View className=" h-14 flex-row">
                   {/* Primeiro quadrado pequeno */}
                     <View className=" h-full w-1/3 justify-start space-y-2">
                       <Text className="text-white font-light text-xs text-center">total de jogos</Text>
-                      <Text className="text-white font-bold text-xs text-center">212</Text>
+                      <Text className="text-white font-bold text-xs text-center">{totalJogos ?? '-'}</Text>
                     </View>
                     {/* Segundo quadrado pequeno */}
                     <View className=" h-full w-1/3 justify-start space-y-2">
                       <Text className="text-white font-light text-xs text-center">Criado em</Text>
-                      <Text className="text-white font-bold text-xs text-center">06/09/2023</Text>
+                      <Text className="text-white font-bold text-xs text-center">{criado ?? "-"}</Text>
                     </View>
                     {/* Terceiro quadrado pequeno */}
                     <View className=" h-full w-1/3 justify-start space-y-2">
                       <Text className="text-white font-light text-xs text-center">Jogos</Text>
-                      <Text className="text-white font-bold text-xs text-center">Sexta</Text>
+                      <Text className="text-white font-bold text-xs text-center">{jogos}</Text>
                     </View>
                 </View>
               </View>
               {/* quadrado abaixo da imagem */}
               <View className=" w-16 h-12 ml-4 mt-20 absolute justify-start space-y-2">
                 <Text className="text-white font-light text-xs text-center">Membros</Text>
-                <Text className="text-white font-bold text-xs text-center">67</Text>
+                <Text className="text-white font-bold text-xs text-center">{membros ?? "-"}</Text>
               </View>
               
 
--- screens/GerenciarGrupo.js
+++ screens/GerenciarGrupo.js
@@ -1,20 +1,93 @@
-import { View, Text, TouchableOpacity, ScrollView, Image } from 'react-native'
-import React from 'react'
+import { View, Text, TouchableOpacity, ScrollView, Image, ActivityIndicator } from 'react-native'
+import React, {useLayoutEffect, useEffect, useState} from 'react'
 import luis from '../assets/luis.png'
+import udd from '../assets/udd.jpg'
 import Grupo from '../components/Grupo'
+import apiUrl from '../config.js';
+import axios, { AxiosError } from 'axios';
+import { useNavigation } from '@react-navigation/native'
+
+
 
 
 const GerenciarGrupo = () => {
+
+  const navigation = useNavigation();
+
+  const [grupos, setGrupos] = useState([]);
+
+  const [loading, setLoading] = useState(true); // Estado de carregamento
+
+
+
+useEffect(() => {
+  // Faz a solicitação à API para obter os nomes
+
+
+  const fetchData = async () => {
+
+    try{
+      //   const token = await AsyncStorage.getItem('token');
+      //   if (token) {
+            const response = await axios.get(`${apiUrl}/api/usuario_grupos/1`);
+
+            
+  
+            setGrupos(response.data.data); // Atualiza o estado com os nomes obtidos da API
+          // }
+          
+          
+      
+    }
+    catch(error){
+        console.log(error)
+        let toast = Toast.show('Requisição não concluida!', {
+          duration: Toast.durations.LONG,
+        });
+    }
+    finally {
+      
+      setLoading(false); // Altera o estado de carregamento para false após a resposta da API
+    }
+    
+  }
+
+  
+    fetchData();
+
+    
+}, []);
+
+if (loading) {
+  // Exibe um indicador de carregamento enquanto espera a resposta da API
+  return (
+    <View className="flex-1 justify-center items-center">
+      <ActivityIndicator size="large" color="orange" />
+    </View>
+  );
+}
+
+
+
   return (
     <View className="flex-1">
       <ScrollView>
         <View className="flex items-center pt-4 flex-col space-y-7">
-            <View>
-              <Grupo />
-            </View>
-            <View>
-              <Grupo />
-            </View>
+            
+          {grupos?.map((el) => {
+              
+               return (
+                <View className="space-y-4">
+                  <Grupo key={el.id} nome={el.nome} totalJogos={658} criado={el.created_at} jogos="Sexta" membros={12} imagem={udd} />
+                </View>
+               ) 
+               
+               
+                
+              
+          })}
+          
+          
             
         </View>
       </ScrollView>
